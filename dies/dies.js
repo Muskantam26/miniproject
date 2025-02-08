aff0efe06d2b7645f2bf09cffd9889b7ca9121ca
Muskantam26 <mskntmrkr@gmail.com>
2025-02-08 17:25:24 +0530

    update

---
+++ dies/dies.js
@@ -0,0 +1,127 @@
+let cc=()=>{
+
+    let di=(Math.floor(Math.random()*6+1));
+    console.log(di);
+   
+    let dd=document.querySelector("#sec")
+    if(di==1){
+ let d1=document.querySelector("#sec1")
+   d1.style.display="block";
+ 
+ 
+   let d2=document.querySelector("#sec2")
+        d2.style.display="none"
+        
+        let d3=document.querySelector("#sec3")
+        d3.style.display="none"
+ 
+        let d4=document.querySelector("#sec4")
+        d4.style.display="none"
+         let d5=document.querySelector("#sec5")
+         d5.style.display="none"
+         let d6=document.querySelector("#sec6")
+         d6.style.display="none"
+ 
+ 
+     }
+     else if(di==2){
+         let d1=document.querySelector("#sec1")
+          d1.style.display="block";
+ 
+         let d2=document.querySelector("#sec2")
+         d2.style.display="block"
+ 
+         let d3=document.querySelector("#sec3")
+         d3.style.display="none"
+  
+         let d4=document.querySelector("#sec4")
+         d4.style.display="none"
+          let d5=document.querySelector("#sec5")
+          d5.style.display="none"
+          let d6=document.querySelector("#sec6")
+          d6.style.display="none"
+         
+     }
+     else if(di==3){
+ 
+         let d1=document.querySelector("#sec1")
+          d1.style.display="block";
+ 
+         let d2=document.querySelector("#sec2")
+         d2.style.display="block"
+         
+         let d3=document.querySelector("#sec3")
+         d3.style.display="block"
+ 
+ 
+         let d4=document.querySelector("#sec4")
+        d4.style.display="none"
+         let d5=document.querySelector("#sec5")
+         d5.style.display="none"
+         let d6=document.querySelector("#sec6")
+         d6.style.display="none"
+ 
+     }
+     else if(di==4){
+ 
+ 
+          let d1=document.querySelector("#sec1")
+          d1.style.display="block";
+ 
+         let d2=document.querySelector("#sec2")
+         d2.style.display="block"
+         
+         let d3=document.querySelector("#sec3")
+         d3.style.display="block"
+ 
+         let d4=document.querySelector("#sec4")
+         d4.style.display="block"
+ 
+         let d5=document.querySelector("#sec5")
+         d5.style.display="none"
+         let d6=document.querySelector("#sec6")
+         d6.style.display="none"
+ 
+     }
+     else if(di==5){
+         let d1=document.querySelector("#sec1")
+         d1.style.display="block";
+ 
+        let d2=document.querySelector("#sec2")
+        d2.style.display="block"
+        
+        let d3=document.querySelector("#sec3")
+        d3.style.display="block"
+ 
+        let d4=document.querySelector("#sec4")
+        d4.style.display="block"
+         let d5=document.querySelector("#sec5")
+         d5.style.display="block"
+ 
+         let d6=document.querySelector("#sec6")
+         d6.style.display="none"
+ 
+     }
+     else if(di==6){
+         let d1=document.querySelector("#sec1")
+         d1.style.display="block";
+ 
+        let d2=document.querySelector("#sec2")
+        d2.style.display="block"
+        
+        let d3=document.querySelector("#sec3")
+        d3.style.display="block"
+ 
+        let d4=document.querySelector("#sec4")
+        d4.style.display="block"
+         let d5=document.querySelector("#sec5")
+         d5.style.display="block"
+         let d6=document.querySelector("#sec6")
+         d6.style.display="block"
+     }
+     
+ 
+ 
+ 
+    
+ }
